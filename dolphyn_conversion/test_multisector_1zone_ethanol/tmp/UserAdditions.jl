b89a4bad5dc02a84a4a48250ae82a22849f20e92
acheng1757 <abbiec1757@gmail.com>
2026-02-28 18:55:30 -0500

    created example assets

---
+++ dolphyn_conversion/test_multisector_1zone_ethanol/tmp/UserAdditions.jl
@@ -0,0 +1,14 @@
+module UserAdditions
+using MacroEnergy
+
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/examples/multisector_3zone/tmp/usersubcommodities.jl"
+if isfile(commodities_path)
+    include(commodities_path)
+end
+
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/examples/multisector_3zone/tmp/userassets.jl"
+if isfile(assets_path)
+    include(assets_path)
+end
+
+end
