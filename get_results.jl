4527056b3067beb7982fbb51ec41867d6548f052
acheng1757 <abbiec1757@gmail.com>
2026-03-02 15:57:20 -0500

    adding more example assets

--- get_results.jl
+++ get_results.jl
@@ -7,9 +7,9 @@ import MacroEnergy: id, find_node, all_constraints_types, get_constraint_by_type
 system = systems[1]
 
 # ---- Results directory ----
-const BASE = "/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_conversion/1zone_SE_8736"
+const BASE = "/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/ethylene_system/multisector_1zone_testing"
 make_path(parts...) = joinpath(BASE, parts...)
-results_dir = make_path("results_003", "results")
+results_dir = make_path("results_001", "results")
 
 print("Adding CO2 cap duals now")
 
