5da1792d3fe89b2704e093e3e9fbf22ed9297128
acheng1757 <abbiec1757@gmail.com>
2026-03-03 23:18:39 -0500

    getting upgrades to work

--- ETHYLENESYSTEM_ethanol+ethylene/liquid_fuels_only/tmp/UserAdditions.jl
+++ ETHYLENESYSTEM_ethanol+ethylene/liquid_fuels_only/tmp/UserAdditions.jl
@@ -1,12 +1,12 @@
 module UserAdditions
 using MacroEnergy
 
-commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/ETHYLENESYSTEM_ethanol/multisector_1zone_ethanol/tmp/usersubcommodities.jl"
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/ETHYLENESYSTEM_ethanol+ethylene/liquid_fuels_only/tmp/usersubcommodities.jl"
 if isfile(commodities_path)
     include(commodities_path)
 end
 
-assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/ETHYLENESYSTEM_ethanol/multisector_1zone_ethanol/tmp/userassets.jl"
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/ETHYLENESYSTEM_ethanol+ethylene/liquid_fuels_only/tmp/userassets.jl"
 if isfile(assets_path)
     include(assets_path)
 end
