c02ea6391b9afdeabf3282da39e10a5814f26fb4
acheng1757 <abbiec1757@gmail.com>
2026-03-13 19:47:12 -0400

    power test works!

---
+++ dolphyn_system/EASTERN_US_MACRO_Ethylene_H2/tmp/UserAdditions.jl
@@ -0,0 +1,14 @@
+module UserAdditions
+using MacroEnergy
+
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene/tmp/usersubcommodities.jl"
+if isfile(commodities_path)
+    include(commodities_path)
+end
+
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene/tmp/userassets.jl"
+if isfile(assets_path)
+    include(assets_path)
+end
+
+end
