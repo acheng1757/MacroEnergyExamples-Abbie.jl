30e4ef9152eef19d20d78e9063419c1af770854f
acheng1757 <abbiec1757@gmail.com>
2026-03-14 20:16:53 -0400

    adding more tests

---
+++ dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC_automated/tmp/UserAdditions.jl
@@ -0,0 +1,14 @@
+module UserAdditions
+using MacroEnergy
+
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC_automated/tmp/usersubcommodities.jl"
+if isfile(commodities_path)
+    include(commodities_path)
+end
+
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC_automated/tmp/userassets.jl"
+if isfile(assets_path)
+    include(assets_path)
+end
+
+end
