3829ff87397347f21fb3ab6ec8a46dcacd62c61e
acheng1757 <abbiec1757@gmail.com>
2026-03-22 16:08:50 -0400

    running examples

---
+++ 3Zone_Ethylene/power_h2/tmp/UserAdditions.jl
@@ -0,0 +1,14 @@
+module UserAdditions
+using MacroEnergy
+
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/3Zone_Ethylene/power_h2/tmp/usersubcommodities.jl"
+if isfile(commodities_path)
+    include(commodities_path)
+end
+
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/3Zone_Ethylene/power_h2/tmp/userassets.jl"
+if isfile(assets_path)
+    include(assets_path)
+end
+
+end
--- dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC+ETHYLENE_globaldemand/tmp/UserAdditions.jl
+++ dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC+ETHYLENE_globaldemand/tmp/UserAdditions.jl
@@ -1,12 +1,12 @@
 module UserAdditions
 using MacroEnergy
 
-commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC_globaldemand/tmp/usersubcommodities.jl"
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC+ETHYLENE_globaldemand/tmp/usersubcommodities.jl"
 if isfile(commodities_path)
     include(commodities_path)
 end
 
-assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC_globaldemand/tmp/userassets.jl"
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/dolphyn_system/EASTERN_US_MACRO_Ethylene_POWER+H2+CSC+NG+LF+BESC+ETHYLENE_globaldemand/tmp/userassets.jl"
 if isfile(assets_path)
     include(assets_path)
 end
