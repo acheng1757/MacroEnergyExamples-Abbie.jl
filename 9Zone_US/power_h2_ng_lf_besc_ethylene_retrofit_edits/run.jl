e033eec371b7128b8e0cf155a69abfbe131c1f55
acheng1757 <abbiec1757@gmail.com>
2026-03-23 16:15:05 -0400

    9 zone ethylene case

---
+++ 9Zone_US/power_h2_ng_lf_besc_ethylene_retrofit_edits/run.jl
@@ -0,0 +1,10 @@
+using MacroEnergy
+using Gurobi
+
+MacroEnergy.retrofitted_capacity(::MacroEnergy.Storage) = 0.0
+
+(system, model) = run_case(
+    @__DIR__;
+    optimizer=Gurobi.Optimizer,
+    optimizer_attributes=("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-2),
+);
