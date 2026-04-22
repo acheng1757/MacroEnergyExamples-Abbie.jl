90cb5e583e7e778cd894901db706323195c6d1f7
acheng1757 <abbiec1757@gmail.com>
2026-04-22 14:53:37 -0400

    beccs_hydrogen not working

---
+++ JunWen_4_18_edited/NineZones_High_Biomass_High_CO2_26_GLOBALLF/tmp/UserAdditions.jl
@@ -0,0 +1,14 @@
+module UserAdditions
+using MacroEnergy
+
+commodities_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/JunWen_4_18_edited/NineZones_High_Biomass_High_CO2_26_GLOBALLF/tmp/usersubcommodities.jl"
+if isfile(commodities_path)
+    include(commodities_path)
+end
+
+assets_path = raw"/Users/abbie/MacroEnergy-Abbie.jl/MacroEnergyExamples/JunWen_4_18_edited/NineZones_High_Biomass_High_CO2_26_GLOBALLF/tmp/userassets.jl"
+if isfile(assets_path)
+    include(assets_path)
+end
+
+end
