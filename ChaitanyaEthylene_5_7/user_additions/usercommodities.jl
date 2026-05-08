3b930e8b11e8c50f66e4aeefeab07457074048ae
acheng1757 <abbiec1757@gmail.com>
2026-05-07 21:44:07 -0400

    Refactor code structure for improved readability and maintainability

--- ChaitanyaEthylene_5_7/user_additions/usercommodities.jl
+++ ChaitanyaEthylene_5_7/user_additions/usercommodities.jl
@@ -9,3 +9,4 @@ abstract type Fossil_Gasoline <: MacroEnergy.LiquidFuels end
 abstract type Fossil_JetFuel <: MacroEnergy.LiquidFuels end
 abstract type Fossil_Diesel <: MacroEnergy.LiquidFuels end
 abstract type Ethanol <: MacroEnergy.LiquidFuels end
+abstract type Upstream_Ethanol <: MacroEnergy.LiquidFuels end
